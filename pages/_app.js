644d25580d208e5fb4b224a787535c1552d18578
dijisolanke <60063252+dijisolanke@users.noreply.github.com>
2021-12-28 13:17:29 +0000

    changes made to .prettierrc.json file, changed semi from true to false

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,13 @@
-import '../styles/globals.css'
-import '../styles/header.css'
-import '../styles/footer.css'
-import '../styles/profileImg.css'
-import '../styles/bundle.css'
-import SiteLayout from '../components/siteLayout'
+import '../styles/globals.css';
+import '../styles/header.css';
+import '../styles/footer.css';
+import '../styles/profileImg.css';
+import '../styles/bundle.css';
+import SiteLayout from '../components/siteLayout';
 import { StyledEngineProvider } from '@mui/material/styles';
 
 
-import Head from 'next/head'
+
 
 
  function MyApp({ Component, pageProps }) {
